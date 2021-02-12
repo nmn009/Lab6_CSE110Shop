e11559b69dc37bd7cc0ca9a06d3b894851644be1
David Cruz <dac012+cse110github@ucsd.edu>
2021-02-11 22:32:41 -0800

    Complete assignment

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,51 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' })
+
+    this.li = document.createElement('li');
+    this.li.setAttribute('class', 'product');
+
+    this.img = this.li.appendChild(document.createElement('img'));
+
+    this._title = this.li.appendChild(document.createElement('p'));
+    this._title.setAttribute('class', 'title');
+
+    this.price = this.li.appendChild(document.createElement('p'));
+    this.price.setAttribute('class', 'price');
+
+    this.button = this.li.appendChild(document.createElement('button'));
+    this.button.setAttribute('onclick', `alert('Added to Cart!')`)
+
+    const styleLink = document.createElement('link');
+    styleLink.setAttribute('rel', 'stylesheet');
+    styleLink.setAttribute('href', './styles/styles.css')
+
+    this.shadowRoot.append(this.li, styleLink);
+  }
+
+  static get observedAttributes() {
+    return ['product', 'in-cart'];
+  }
+
+  attributeChangedCallback(name, prev, val) {
+    if (name === 'product') {
+      this.product = JSON.parse(val);
+      this.img.setAttribute('src', this.product.image);
+      this.img.setAttribute('alt', this.product.title);
+
+      this._title.innerText = this.product.title;
+
+      this.price.innerText = '$' + this.product.price.toFixed(2);
+    } else if (name === 'in-cart') {
+      const inCart = val === 'true';
+
+      this.button.innerText = !inCart ? 'Add to Cart' : 'Remove from Cart';
+      this.button.setAttribute('onclick', !inCart ? `addToCart(${this.product.id})` : `removeFromCart(${this.product.id})`);
+    }
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,53 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+let cart = [];
+let products = [];
+
+window.addEventListener('DOMContentLoaded', async () => {
+  if (window.localStorage.getItem('products')) {
+    products = JSON.parse(window.localStorage.getItem('products'));
+  } else {
+    const res = await fetch('https://fakestoreapi.com/products');
+    products = await res.json();
+    window.localStorage.setItem('products', JSON.stringify(products));
+  }
+
+  if (window.localStorage.getItem('cart')) {
+    cart = JSON.parse(window.localStorage.getItem('cart'));
+  } else {
+    cart = [];
+    saveCart();
+  }
+
+  redrawProducts();
 });
+
+function redrawProducts() {
+  const container = document.getElementById('product-list');
+  // clear children
+  container.innerHTML = null;
+  // readd
+  for (const product of products) {
+    const elem = document.createElement('product-item');
+    elem.setAttribute('product', JSON.stringify(product));
+    elem.setAttribute('in-cart', cart.includes(product.id).toString())
+    container.appendChild(elem);
+  }
+  document.getElementById('cart-count').innerText = cart.length.toString();
+}
+
+function saveCart() {
+  window.localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function addToCart(id) {
+  cart.push(id);
+  redrawProducts();
+  saveCart();
+}
+
+function removeFromCart(id) {
+  cart = cart.filter(existing => existing !== id);
+  redrawProducts();
+  saveCart();
+}
